989f470c2681a4a52da236b7162b52489e5837b8
Atixson <Atixson37@gmail.com>
2023-03-11 12:51:27 +0300

    Separation DONE

---
+++ Arrays/FillRand.cpp
@@ -0,0 +1,65 @@
+#include"FillRand.h"
+
+void FillRand(int arr[], const int n, int minRand, int maxRand)
+{
+	//Заполнение массива случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+}
+void FillRand(double arr[], const int n, int minRand, int maxRand)
+{
+	//Заполнение массива случайными числами:
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr[i] /= 100;
+	}
+}
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+		}
+	}
+}
+void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
+{
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = double(rand() % (maxRand - minRand) + minRand) / 100;
+		}
+	}
+}
+
+void UniqueRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		bool unique;	//Флаг уникальности
+		do
+		{
+			arr[i] = rand() % n;
+			unique = true;	//Предполагаем что случайное число уникально,
+			//но это нужно проверить:
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;	//Ключевое слово break прерывает текущую итерацию, и все последующие.
+				}
+			}
+		} while (!unique);
+	}
+}
---
+++ Arrays/constants.h
@@ -0,0 +1,5 @@
+//constants.h
+#pragma once
+
+const int ROWS = 5;	//Количесво строк
+const int COLS = 8;	//Количество элементов строки (столбцов)
